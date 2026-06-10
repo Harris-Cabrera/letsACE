4d1266e674fc4d3b3997971e8e3cf96c9f16af35
Harris <harris1210@hotmail.com>
2026-06-09 23:24:56 -0400

    Implement user registration endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 
 from app.database import engine
 from app import models
+from app.routes import auth_routes
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="letsACE API")
+
+app.include_router(auth_routes.router)
 
 
 @app.get("/")
