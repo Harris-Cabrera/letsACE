72f3f801fd38e050ac3f53865f9e6605730c1e2b
Harris <harris1210@hotmail.com>
2026-06-10 23:22:26 -0400

    Create queestion database model

--- backend/app/models.py
+++ backend/app/models.py
@@ -11,3 +11,17 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+
+class Question(Base):
+    __tablename__ = "questions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    domain = Column(String, index=True, nullable=False)
+    question_text = Column(String, nullable=False)
+    option_a = Column(String, nullable=False)
+    option_b = Column(String, nullable=False)
+    option_c = Column(String, nullable=False)
+    option_d = Column(String, nullable=False)
+    correct_answer = Column(String, nullable=False)
+    explanation = Column(String, nullable=False)
