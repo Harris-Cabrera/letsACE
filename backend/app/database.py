9ae8ffadc6c314aa2d48ea97e8e7b0b0b75513af
Harris <harris1210@hotmail.com>
2026-07-05 17:47:50 -0400

    Completed deployment configation

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,11 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from app.config import DATABASE_URL
+
 import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 
