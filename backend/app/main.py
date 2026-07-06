4ad4ada252e3eff9786302ccf530d1f3b2a20291
Harris <harris1210@hotmail.com>
2026-07-06 19:17:42 -0400

    implemented new Domain name directory

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,6 +21,8 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://lets-ace.vercel.app",
+        "https://letsace.app",
+        "https://www.letsace.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
